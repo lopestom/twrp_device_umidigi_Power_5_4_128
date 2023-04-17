77790ee1b94df18def3ece403228c31010beff1d
lopestorm <guillopes2002@gmail.com>
2023-04-17 00:53:16 -0300

    Add files via upload

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,8 +69,7 @@ AB_OTA_PARTITIONS += \
 # File systems and partitions
 BOARD_HAS_LARGE_FILESYSTEM := true
 #BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
-#BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2000000
-BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2800000
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x2000000
 
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 0x53788000
@@ -78,12 +77,15 @@ BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_VENDORIMAGE_PARTITION_SIZE := 0x1ECF9000
 BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 0x1B96CF8000
-
-# Workaround for error copying files to recovery ramdisk
-TARGET_COPY_OUT_VENDOR := vendor
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_PARTITION_SIZE := 681574400
 
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
+
+# Workaround for error copying files to recovery ramdisk
+TARGET_COPY_OUT_VENDOR := vendor
+TARGET_COPY_OUT_PRODUCT := product
 
 # Dynamic Partition
 BOARD_SUPER_PARTITION_SIZE := 6442450944
@@ -122,7 +124,8 @@ BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_KERNEL_PAGESIZE) --board ""
 BOARD_MKBOOTIMG_ARGS += --dtb_offset $(BOARD_DTB_OFFSET)
 
 # Hack to get keymaster to recognize the key files
-PLATFORM_SECURITY_PATCH := 2021-06-05
+PLATFORM_SECURITY_PATCH := 2099-12-05
+#PLATFORM_SECURITY_PATCH := 2021-06-05
 PLATFORM_VERSION := 11
 VENDOR_SECURITY_PATCH := 2021-06-05
 
@@ -135,23 +138,21 @@ TW_HAS_NO_RECOVERY_PARTITION := true
 
 # Metadata
 BOARD_USES_METADATA_PARTITION := true
+BOARD_ROOT_EXTRA_FOLDERS += metadata
 
 # Crypto
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
-TW_INCLUDE_FBE := true
+#TW_INCLUDE_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
 TW_USE_FSCRYPT_POLICY := 2
-#TW_USE_FSCRYPT_POLICY := 1
-#TW_CRYPTO_USE_SYSTEM_VOLD := true
+TW_PREPARE_DATA_MEDIA_EARLY := true
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
     libpuresoftkeymasterdevice
 
 TW_RECOVERY_ADDITIONAL_RELINK_LIBRARY_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
     $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
 # Properties
@@ -181,12 +182,11 @@ TARGET_RECOVERY_INITRC := $(DEVICE_PATH)/recovery/root/init.recovery.mt6762.rc
 RECOVERY_SDCARD_ON_DATA := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_INCLUDE_NTFS_3G := true
-#TW_USE_TOOLBOX := true
-TW_USE_TOYBOX := true # should make use of ToyBox rather than BusyBox or ToolBox which are deprecated
+TW_USE_TOOLBOX := true
 TW_DEFAULT_LANGUAGE := en
 TW_EXTRA_LANGUAGES := false
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
-TW_DEVICE_VERSION := Umi Power 5_N_V1_220315-4_128 - encrypted
+TW_DEVICE_VERSION := UmiPower 5_N_V1_220315-4_128 - lopestom
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone1/temp
 TARGET_RECOVERY_LCD_BACKLIGHT_PATH := \"/sys/class/leds/lcd-backlight/brightness\"
 TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
@@ -195,23 +195,22 @@ TW_DEFAULT_BRIGHTNESS := 155
 #TW_SCREEN_BLANK_ON_BOOT := true
 TW_NO_SCREEN_BLANK := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-#TW_NO_USB_STORAGE := false
+TW_NO_USB_STORAGE := false
 TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
-#TW_USE_SERIALNO_PROPERTY_FOR_DEVICE_ID := true
 
 TW_HAS_MTP := true
-
-#TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_MTP_DEVICE := /dev/mtp_usb
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
 TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
 TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_MTP_DEVICE := /dev/mtp_usb
-#TW_DEFAULT_EXTERNAL_STORAGE := false
-#TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_EXCLUDE_TWRPAPP := true
+TW_DEFAULT_EXTERNAL_STORAGE := true
 
-#TW_EXCLUDE_APEX := true
+TW_EXCLUDE_TWRPAPP := true
+TW_EXCLUDE_APEX := true
+TW_EXCLUDE_PYTHON := true
+TW_EXCLUDE_NANO := true
+
 TW_INCLUDE_RESETPROP := true
 TARGET_USES_MKE2FS := true
 
@@ -219,17 +218,24 @@ TARGET_USES_MKE2FS := true
 TW_THEME := portrait_hdpi
 DEVICE_SCREEN_WIDTH := 720
 DEVICE_SCREEN_HEIGHT := 1600
-TW_Y_OFFSET  := 55
-TW_H_OFFSET  := -55
+#TW_Y_OFFSET  := 55
+#TW_H_OFFSET  := -55
+
+# Statusbar icons flags 720 x 1600
+TW_STATUS_ICONS_ALIGN := center
+#TW_CUSTOM_CPU_POS := 50
+TW_CUSTOM_CLOCK_POS := 300
+#TW_CUSTOM_BATTERY_POS := 800
+
 # Debug
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
+#TW_CRYPTO_FS_TYPE := "ext4"
+#TW_CRYPTO_REAL_BLKDEV := "/dev/block/platform/bootdevice/by-name/userdata"
 #TW_CRYPTO_REAL_BLKDEV := "/dev/block/dm-8"
-TW_CRYPTO_MNT_POINT := "/data"
+#TW_CRYPTO_MNT_POINT := "/data"
 #TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,discard,noauto_da_alloc,data=ordered"
 #TW_CRYPTO_FS_OPTIONS := "rw,seclabel,nosuid,nodev,noatime,noauto_da_alloc,inlinecrypt,resgid=1065,errors=panic,data=ordered"
-TW_CRYPTO_FS_OPTIONS := "rw,seclabel,nosuid,nodev,noatime,noauto_da_alloc,inlinecrypt,resuid=10010,resgid=1065,errors=panic 0 0"
-TW_CRYPTO_KEY_LOC := /metadata/vold/metadata_encryption/key
+#TW_CRYPTO_FS_OPTIONS := "rw,seclabel,nosuid,nodev,noatime,noauto_da_alloc,inlinecrypt,resuid=10010,resgid=1065,errors=panic 0 0"
+#TW_CRYPTO_KEY_LOC := /metadata/vold/metadata_encryption/key
